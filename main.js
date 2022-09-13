962b0f44f696a52e7de87bd42cdca7a1090553e3
siva <sivashakthiarun.c@gmail.com>
2022-09-13 10:27:33 +0530

    repush

---
+++ main.js
@@ -0,0 +1,7 @@
+var fabevy=["Don","Shakthi","Ajay","Wikki"];
+console.log("Original array:",fabevy);
+console.log("Length of original array:",fabevy.length);
+var a=fabevy.join(" ");
+console.log("Method:fabevy.join('');");
+console.log("Change in array:",a);
+console.log("Return:Length of the array after indexOf(); is"+a.length);
